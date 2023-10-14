a9067b69dfb8c81061552773d52277a2766548b0
Abdul Sammi Gul <sammigul11005@gmail.com>
2023-10-14 21:32:01 +0500

    set up image analysis

--- main.py
+++ main.py
@@ -3,22 +3,14 @@ import numpy as np
 import dlib
 import cv2.aruco as aruco
 
-from aruco_utils import calculate_pixel_mm_ratio
 from ipd_image import imageAnalysis
 from ipd_video import live_video_analysis
-from utils import midpoint
-from utils import template_matching
-'''
-Using template matching to match the credit card in the image
-
-'''
-
 
 
 # For Live Video 
 # live_video_analysis(75,aruco.DICT_4X4_100)
 
 # For image only
-image_path = 'assets/images/test1.jpeg'
+image_path = 'assets/images/4.jpg'
 
 print(f"IPD Distance in mm  {imageAnalysis(image_path,75,aruco.DICT_4X4_100)}")
